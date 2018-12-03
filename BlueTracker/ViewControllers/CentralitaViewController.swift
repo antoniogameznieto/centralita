5c5d8f747761fb17d7033201ad829d09a3d07331
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-03 22:53:22 +0100

    Pintando el viewcontroller de la selección de bluetooth

---
+++ BlueTracker/ViewControllers/CentralitaViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CentralitaViewController.swift
+//  BlueTracker
+//
+//  Created by antoniogamez on 3/12/18.
+//  Copyright © 2018 Adhoc. All rights reserved.
+//
+
+import UIKit
+
+class CentralitaViewController: UIViewController {
+
+    @IBOutlet weak var etiquetaConexion: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
