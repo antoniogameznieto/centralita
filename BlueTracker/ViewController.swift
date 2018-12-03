47995efbdd63c6adde5278cd5a9d91e383398a72
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-03 18:32:40 +0100

    Initial Commit

---
+++ BlueTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueTracker
+//
+//  Created by antoniogamez on 3/12/18.
+//  Copyright © 2018 Adhoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
